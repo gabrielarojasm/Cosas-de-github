ad9eaab60e97dcc9ba919aa94403eb7c087b3d5d
gabrielarojasm <gabriela.rojas.012@gmail.com>
2020-05-29 22:44:33 -0400

    ultimo

---
+++ hw_09/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "funciones.h"
+
+int main(void){ 
+    {printf("\nThe value of y at x is : %f", 
+             rungeKutta(0,8.5, x, 0.1, dydt);
+        return 0;}
+    
+
+
+    {printf("\nThe value of y at x is : %f", 
+             rungeKutta(0,8.5, x, 0.1, dvdt);
+        return 0;
+     } 
+    
+}
